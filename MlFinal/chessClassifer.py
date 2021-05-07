7c7b45cde55708ecb6d54a5e472c3395e0f00079
haertling <stevenhaertling@yahoo.com>
2021-05-06 20:13:00 -0500

    moving to process data a bit

--- MlFinal/chessClassifer.py
+++ MlFinal/chessClassifer.py
@@ -1,8 +1,8 @@
 '''
     File name: chessClassifier.py
     Author: Steven Haertling
-    Date created: 4/24/2021
-    Date last modified: 4/24/2021
+    Date created: 04/24/2021
+    Date last modified: 05/05/2021
     Python Version: 3.8.6
 '''
 
@@ -26,7 +26,7 @@ import time
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-
+g_list = list()
 
 def processKRKP( ):
     # read from file
@@ -41,20 +41,6 @@ def processKRKP( ):
     x_train, x_test, y_train, y_test = train_test_split( matrix[:, 1:37], matrix[:, 37], test_size=testSize, random_state=4 )
     return x_train, x_test, y_train, y_test
 
-
-def processKRK( S ):
-
-    enc = preprocessing.OrdinalEncoder()
-    # X = [['a', 1],['b',2],['c',3],['d',4],['e',5],['f',6],['g',7],['h',8]]
-    # enc.fit(X)
-    matrix = enc.fit_transform(S[:, 0:6])
-    le = preprocessing.LabelEncoder()
-    col_vector = le.fit_transform(S[:,6])
-    # np.set_printoptions(threshold=sys.maxsize)
-    # print(S[:,6])
-    # print(col_vector)
-    return matrix, col_vector
-
 def decision_tree_driverKRvKP( x_train, x_test, y_train, y_test ):
     
     # run the classifer a bunch with didfferent parameters 
@@ -62,43 +48,59 @@ def decision_tree_driverKRvKP( x_train, x_test, y_train, y_test ):
     features = [1,2,3,4,5,10,15]
     for depth in depths:    
         for feature in features:
+            t0 = time.time()
             d_tree = tree.DecisionTreeClassifier(max_depth=depth,max_features=feature)
             decision_tree = d_tree.fit(x_train, y_train)
             prediction = decision_tree.predict(x_test)
             tn, fp, fn, tp = confusion_matrix(y_test, prediction).ravel()
             error = accuracy_score( y_test, prediction )
-            print("max depth = {}, feature splits = {}".format(depth, feature))
-            print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
-            print("accuracy = {}".format(error))
+            t1 = time.time()
+            runTime = t1 - t0
+            myTuple = [depth, feature, tp,fn,fp,tn,error, runTime, "dTree"]
+            g_list.append(myTuple)
+            # print("max depth = {}, feature splits = {}, runTime = {}".format(depth, feature, runTime))
+            # print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
+            # print("accuracy = {}".format(error))
+    # print(g_list)
 
 def bagging_dtree_driverKRVKP( x_train, x_test, y_train, y_test ):
     depths = [1,2,3,5,7,10]
     bag_sizes = [1,3,5,10,15]
     for depth in depths:    
         for bag_size in bag_sizes:
+            t0 = time.time()
             sBag = BaggingClassifier( base_estimator=tree.DecisionTreeClassifier(max_depth=depth, max_features=1),
                                       n_estimators=bag_size).fit( x_train, y_train )
             prediction = sBag.predict(x_test)
             tn, fp, fn, tp = confusion_matrix(y_test, prediction).ravel()
             error = accuracy_score( y_test, prediction )
-            print("max depth = {}, bag size = {}".format(depth, bag_size))
-            print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
-            print("accuracy = {}".format(error))
+            t1 = time.time()
+            runTime = t1 - t0
+            myTuple = [depth, bag_size, tp,fn,fp,tn,error, runTime, "bagging"]
+            g_list.append(myTuple)
+            # print("max depth = {}, bag size = {}, runTime = {}".format( depth, bag_size, runTime ))
+            # print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
+            # print("accuracy = {}".format(error))
 
 def boosting_dtree_driverKRVKP( x_train, x_test, y_train, y_test ):
     depths = [1,2]
     bag_sizes = [10,20,30,40,50,60]
     for depth in depths:    
         for bag_size in bag_sizes:
+            t0 = time.time()
             sBoost = AdaBoostClassifier( base_estimator=tree.DecisionTreeClassifier(max_depth=depth, max_features=1),
                                          n_estimators=bag_size)
             sBoost.fit(x_train,y_train)
             prediction = sBoost.predict(x_test)
             tn, fp, fn, tp = confusion_matrix(y_test, prediction).ravel()
             error = accuracy_score( y_test, prediction )
-            print("max depth = {}, bag size = {}".format(depth, bag_size))
-            print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
-            print("accuracy = {}".format(error))
+            t1 = time.time()
+            runTime = t1 - t0
+            myTuple = [depth, bag_size, tp,fn,fp,tn,error, runTime, "boosting"]
+            g_list.append(myTuple)
+            # print("max depth = {}, bag size = {}, runTime = {}".format(depth, bag_size, runTime))
+            # print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
+            # print("accuracy = {}".format(error))
 
 def knn_driver_KRVKP( x_train, x_test, y_train, y_test ):
     weights = ['uniform', 'distance']
@@ -116,9 +118,11 @@ def knn_driver_KRVKP( x_train, x_test, y_train, y_test ):
                     error = accuracy_score( y_test, prediction )
                     t1 = time.time()
                     runTime = t1 - t0
-                    print("algorithm = {}, weight = {}, k-nn = {}, leaf = {}, time = {}".format(alg, weight, num_neigh, leaf, runTime))
-                    print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
-                    print("accuracy = {}".format(error))
+                    myTuple = [alg, weight, num_neigh, leaf, tp,fn,fp,tn,error, runTime, "knn"]
+                    g_list.append(myTuple)
+                    # print("algorithm = {}, weight = {}, k-nn = {}, leaf = {}, time = {}".format(alg, weight, num_neigh, leaf, runTime))
+                    # print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
+                    # print("accuracy = {}".format(error))
 
 def logReg_driver_KRVKP( x_train, x_test, y_train, y_test ):
 
@@ -133,22 +137,30 @@ def logReg_driver_KRVKP( x_train, x_test, y_train, y_test ):
             error = accuracy_score( y_test, prediction )
             t1 = time.time()
             runTime = t1 - t0
-            
-            print("algorithm = {}, C = {}, time = {}".format(solver, C, runTime))
-            print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
-            print("accuracy = {}\n".format(error))
+            myTuple = [solver, C, tp,fn,fp,tn,error,runTime,"log"]
+            g_list.append(myTuple)
+            # print("algorithm = {}, C = {}, time = {}".format(solver, C, runTime))
+            # print("true positive  = {} false negative = {}\nfalse positive = {}  true negative = {}".format(tp,fn,fp,tn))
+            # print("accuracy = {}\n".format(error))
 
 if __name__ == '__main__':
+    # Decision Trees, Bagging, Boosting, Logistic Regression, KNN
     # process data into scikit usable form
+    t0 = time.time()
     x_train, x_test, y_train, y_test = processKRKP()
     # x_train, x_test, y_train, y_test = processKRKP2()
     # # use data in normal dtree
-    # decision_tree_driverKRvKP( x_train, x_test, y_train, y_test )
-    # # use data in bagging dtree
-    # bagging_dtree_driverKRVKP( x_train, x_test, y_train, y_test )
-    # # use data in boosting dtree
-    # boosting_dtree_driverKRVKP( x_train, x_test, y_train, y_test )
-    # # use data in knn
-    # knn_driver_KRVKP( x_train, x_test, y_train, y_test )
+    decision_tree_driverKRvKP( x_train, x_test, y_train, y_test )
+    # use data in bagging dtree
+    bagging_dtree_driverKRVKP( x_train, x_test, y_train, y_test )
+    # use data in boosting dtree
+    boosting_dtree_driverKRVKP( x_train, x_test, y_train, y_test )
+    # use data in knn
+    knn_driver_KRVKP( x_train, x_test, y_train, y_test )
     # use data in logistic regression classifier
     logReg_driver_KRVKP( x_train, x_test, y_train, y_test )
+    t1 = time.time()
+    runTime = t1 - t0
+    print("runTime = {}".format(runTime))
+    for myTuple in g_list:
+        print(myTuple)
